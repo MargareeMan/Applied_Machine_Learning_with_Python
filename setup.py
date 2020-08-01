ef56d503faa51ec7515ee90f2d006f4d73d5079e
George Ellis <george@deepdivebi.ca>
2020-08-01 13:05:54 -0300

    Update setup.py
    Changed requirement versions to the versions in my PyCharm project

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     url="https://github.com/andreagiussani/Applied_Machine_Learning_with_Python",
     license="BSD",
     packages=find_packages(),
-    install_requires=['xgboost==0.82', 'pandas==0.23.1', 'scikit-learn==0.20.4',
-                      'seaborn==0.9.0', 'catboost==0.23.2'],
+    install_requires=['xgboost==0.82', 'pandas==1.0.5', 'scikit-learn==0.23.1',
+                      'seaborn==0.10.1', 'catboost==0.23.2'],
     include_package_data=True,
     classifiers=("Programming Language :: Python :: 3"),
 )
